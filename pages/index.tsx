f31b31e20ffd6428a0098012bce100c4399c20fa
SrTinny <joaovictorcardosoduartejoao@gmail.com>
2025-06-29 16:13:41 -0300

    add: melhorando seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,27 +10,33 @@ export default function Home() {
   return (
     <>
       <Head>
+        {/* SEO base */}
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>DevHub | Sites, Sistemas e Impressão em Sobral</title>
         <meta name="description" content="Desenvolvemos sites profissionais, sistemas sob medida, design gráfico e impressão. Atendemos Sobral e toda a região com soluções digitais completas." />
         <meta name="keywords" content="DevHub, sites em Sobral, sistemas web, design gráfico, impressão, desenvolvimento web, software personalizado" />
         <meta name="author" content="DevHub" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="canonical" href="https://devhub-lake.netlify.app/" />
+        <link rel="icon" href="/assets/favicon.ico" />
 
-        {/* Open Graph (Facebook e redes) */}
+        {/* Open Graph (para Facebook, WhatsApp, etc.) */}
         <meta property="og:title" content="DevHub | Sites, Sistemas e Impressão em Sobral" />
         <meta property="og:description" content="Desenvolvemos sites profissionais, sistemas sob medida, design gráfico e impressão." />
         <meta property="og:url" content="https://devhub-lake.netlify.app/" />
         <meta property="og:image" content="https://devhub-lake.netlify.app/assets/logo.png" />
         <meta property="og:type" content="website" />
 
-        {/* Twitter */}
+        {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="DevHub | Sites, Sistemas e Impressão em Sobral" />
         <meta name="twitter:description" content="Sites, sistemas e design com qualidade profissional." />
         <meta name="twitter:image" content="https://devhub-lake.netlify.app/assets/logo.png" />
-      </Head>
 
+        {/* Google Search Console (caso tenha verificação) */}
+        {/* <meta name="google-site-verification" content="SEU_CODIGO_AQUI" /> */}
+      </Head>
 
       <Header />
       <Hero />
